e9f4e469085ad9e93b0df8237ece0b65bbc54666
Andrew Mukiza <mandy.rgf@gmail.com>
2013-08-29 09:22:32 +0300

    Refactoring using a hash to store common symbols

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -1,17 +1,17 @@
 class RomanNumerals
 
+    SYMBOLS = [["V", 5], ["IV", 4], ["I", 1] ]
     def to_roman(number)
-        return "V" if number == 5
-        return "IV" if number == 4
-        result = ""
 
-        while number >= 1
-            result += "I"
-            number -=1
+        result =""
+
+        SYMBOLS.each do |roman,arabic|
+            while number >= arabic
+            result += roman
+            number -= arabic
+            end
         end
 
-        result
-
+     result
     end
-
 end
